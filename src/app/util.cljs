1cf97be42665cc6d257fbdbab8be5443cfe4d0a7
jiyinyiyong <jiyinyiyong@gmail.com>
2018-05-12 23:28:35 +0800

    replace luxon with dayjs

--- src/app/util.cljs
+++ src/app/util.cljs
@@ -1,8 +1,8 @@
 
-(ns app.util (:require ["luxon" :refer [DateTime]]))
+(ns app.util (:require ["dayjs" :as dayjs]))
 
 (defn find-first [f xs] (reduce (fn [_ x] (when (f x) (reduced x))) nil xs))
 
-(defn get-date [] (.toFormat (.local DateTime) "yyyy-MM-dd"))
+(defn get-date [] (.format (dayjs) "YYYY-MM-DD"))
 
 (defn try-verbosely! [x] (try x (catch js/Error e (.error js/console e))))
